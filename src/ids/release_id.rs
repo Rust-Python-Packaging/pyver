293ad297d473e6980335ab48178f59121814186c
Allstreamer <48365544+Allstreamer@users.noreply.github.com>
2022-09-04 16:36:35 +0200

    [`#2`] Moved tests to their respective files (#11)
    closes #2

--- src/ids/release_id.rs
+++ src/ids/release_id.rs
@@ -8,3 +8,25 @@ pub struct ReleaseHeader {
     /// Minor release Such as new functionality
     pub minor: u32,
 }
+
+#[cfg(test)]
+mod test {
+    use super::ReleaseHeader;
+
+    #[test]
+    fn test_release_ordering() {
+        assert!(
+            ReleaseHeader { major: 1, minor: 0 } > ReleaseHeader { major: 0, minor: 0 }
+        );
+        assert!(
+            ReleaseHeader { major: 1, minor: 1 } > ReleaseHeader { major: 1, minor: 0 }
+        );
+        assert!(
+            ReleaseHeader { major: 2, minor: 1 }
+                > ReleaseHeader {
+                    major: 1,
+                    minor: 52,
+                }
+        );
+    }
+}
